60121bd3207c9367f79239fb339374bce81111cc
SpaceMCNetwork <danielgaymer12321@gmail.com>
2026-08-10 18:03:10 -0500

    Cobblemon Champions

---
+++ generar-mods.ps1
@@ -0,0 +1,27 @@
+$baseUrl = "https://sharkz-dev.github.io/atlanticmc.github.io"
+$outputFile = "mods.json"
+
+$items = @()
+
+Get-ChildItem -Path ".\mods-cobblemon" -Filter "*.jar" -File | ForEach-Object {
+    $hash = (Get-FileHash -Algorithm MD5 $_.FullName).Hash.ToLower()
+    $size = $_.Length
+    $name = $_.BaseName
+
+    $items += @{
+        id = "mod:$name"
+        name = $name
+        type = "FabricMod"
+        required = @{
+            value = $true
+            def   = $true
+        }
+        artifact = @{
+            size = $size
+            MD5  = $hash
+            url  = "$baseUrl/mods-cobblemon/$($_.Name)"
+        }
+    }
+}
+
+$items | ConvertTo-Json -Depth 6 | Out-File $outputFile -Encoding UTF8
